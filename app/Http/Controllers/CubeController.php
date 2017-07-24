165928d82b0fc4849d1c5b9c226492416266e893
Jhon Trejos <jhontrl0.1@gmail.com>
2017-07-23 20:20:47 -0500

    first commit

---
+++ app/Http/Controllers/CubeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Classes\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Requests;
+
+class CubeController extends Controller
+{
+    private $results = array();
+
+    public function index(Request $request)
+    {
+    	//If there is nothing in the input, it will return the home(welcome view)
+		if( empty($request->input) ){
+			return view('welcome',['noInput' => 'Nothing to submit.']);
+		}
+
+		$userInput = $this->inputToArray($request->input);
+		$count = 0;
+		$T = (int) $userInput[$count++];
+		// loop each test case 
+		for ($i=0; $i < $T ; $i++) {
+			$tmp = explode(' ', $userInput[$count++]);
+			$N = (int) $tmp[0]; // Get N matrix
+			$M = (int) $tmp[1]; // Get operations
+			$commands = array_slice($userInput,$count,$M);
+			// Do a test case
+			$testCase = new TestCase($N,$M,$commands);
+			$testCase->run();
+			$count = $count + $M;
+			// fecth the results 
+			foreach ($testCase->results() as $result) {
+				$this->results[] = $result;
+			}
+		}
+		// return view and results
+		return view('welcome',['results' => $this->results]);
+    }
+
+    private function inputToArray($input)
+    {
+		$text = trim($input);
+		// get each line into an array
+		$textArray = explode("\n", $text);
+
+		return array_filter($textArray, 'trim');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,78 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Cube Summation</title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+         <script type="text/javascript">
+            function insertText(elemID, text)
+            {
+              var elem = document.getElementById(elemID);
+              elem.innerHTML = text;
+            }
+            var sampleInput = "2 \n" +
+                              "4 5 \n" +
+                              "UPDATE 2 2 2 4 \n" +
+                              "QUERY 1 1 1 3 3 3 \n" +
+                              "UPDATE 1 1 1 23 \n" +
+                              "QUERY 2 2 2 4 4 4 \n" +
+                              "QUERY 1 1 1 3 3 3 \n" +
+                              "2 4 \n" +
+                              "UPDATE 2 2 2 1 \n" +
+                              "QUERY 1 1 1 1 1 1 \n" +
+                              "QUERY 1 1 1 2 2 2 \n" +
+                              "QUERY 2 2 2 2 2 2 \n";
+          </script>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+          <style type="text/css">
+              .row{
+                padding-top: 20px;
+              }
+              button.btn{
+                font-weight: bold;
+              }
+          </style>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+          <div class="container-fluid">
+                
+                <form class="form-horizontal" action="{{ url('cube') }}" method="post">
+                  {{ csrf_field() }}
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h1>Cube Summation</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">   
+                            <textarea name="input" id="input" rows="20" cols="40" class="form-control">
+                            @if( !empty($results) )
+                              @foreach( $results as $result )
+                              {{ $result }}
+                              @endforeach
+                            @elseif (!empty($noInput))
+                                {{ $noInput }}
+                            @endif
+                            </textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6" style="text-align:right;">
+                            <button type="button" id="sampleInputBtn"
+                            class="btn btn-default" onclick="insertText('input', sampleInput);">
+                            Sample Input
+                            </button>
+                            <button type="submit" class="btn btn-success">Submit</button>                            
+                        </div>
+                    </div>
+                </form>
+                
+          </div>
     </body>
 </html>
